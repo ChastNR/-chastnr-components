f8f852f3a2960da676d437bf39f8918325ba9f51
Tihon <38108567+ChastNR@users.noreply.github.com>
2022-05-18 03:12:40 +0300

    v0.0.53

--- src/Control/Control.tsx
+++ src/Control/Control.tsx
@@ -37,7 +37,7 @@ export const Control = forwardRef<HTMLDivElement, PropsWithChildren<ControlProps
     },
     ref
   ) => {
-    const controlClassNames = classNames(className, 'ctrl__cntr', {
+    const controlClassNames = classNames('ctrl__cntr', className, {
       required,
       disabled,
       error,
